56e1744eea14df8d9b6fc0dfeef3ebcfcace3469
Opilane TTHK <opilane@tthk.ee>
2021-11-25 10:25:42 +0200

    Week13

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+namespace OOPPart1
+{
+    class Program
+    {
+        class Dog
+        {
+            string name;
+            int sports;
+            double happiness;
+
+            public Dog(string _name, int _sports)
+            {
+                name = _name;
+                sports = _sports;
+                happiness = 0;
+                Console.WriteLine($"The dog {name} with {sports} has been created.");
+            }
+
+            public string Name
+            {
+                get { return Name; }
+            }
+            public int Sports
+            {
+                get { return Sports; }
+            }
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+
+            public void barks()
+            {
+                Console.WriteLine("woof, woof");
+                happiness += 0.2; //happiness = happiness + 0.2
+            }
+
+            public void WagTail()
+            {
+                Console.WriteLine("wiggle-wiggle");
+            }
+
+            public void Rename(string newName)
+            {
+                name = newName;
+            }
+
+
+
+            static void Main(string[] args)
+            {
+                Dog myDog = new Dog("Koer", 15);
+                //myDog.happiness = 10;
+                //myDog.name = "Good dog";
+
+                while (myDog.happiness != 1)
+                {
+                    myDog.barks();
+                }
+
+                myDog.WagTail();
+                Console.WriteLine("Rename your dog");
+                string userInput = Console.ReadLine();
+                myDog.Rename(userInput);
+                Console.WriteLine($"Name{myDog}");
+                Console.WriteLine($"Level of happiness {myDog.happiness}");
+            }
+        }
+        
+    }
+}
---
+++ OOPCat/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace OOPCat
+{
+    class programm
+    {
+        class Cat
+        {
+
+            string name;
+            string color;
+            double hungry;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hungry = 0;
+                Console.WriteLine($"The cat {name} with {color} has been created.");
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public string Color
+            {
+                get { return color; }
+            }
+
+            public double Hungry
+            {
+                get { return hungry; }
+            }
+
+            public void Sleep()
+            {
+                Console.WriteLine("zzz");
+                hungry += 0.2;
+            }
+
+            public void Meow()
+            {
+                Console.WriteLine("Meow");
+            }
+
+            static void Main(string[] args)
+            {
+                Cat myCat = new Cat("Caroly", "Elise");
+
+                while (myCat.Hungry != 1)
+                {
+                    myCat.Sleep();
+                }
+                myCat.Meow();
+
+                Console.WriteLine($"name: {myCat.Name}");
+                Console.WriteLine($"Level of hungriness: {myCat.Hungry}");
+            }
+
+        }
+
+    }
+}
